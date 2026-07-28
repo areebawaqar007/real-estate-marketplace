57a7d930b5fff16cc25d3ebfbb32f2e2486002f0
Areeba Waqar <areebawaqar007@gmail.com>
2026-07-28 09:20:32 +0500

    Create user model

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 
 import express from "express";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 export default app;
