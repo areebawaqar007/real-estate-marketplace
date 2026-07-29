755481c92a066bfac434d75af71d394ea0145797
Areeba Waqar <areebawaqar007@gmail.com>
2026-07-30 03:40:43 +0500

    Create update user API route

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,11 @@
 import express from "express";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
