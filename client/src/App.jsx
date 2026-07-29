01b38329ee16aa3d97297b6a85f2f8cdba26f413
Areeba Waqar <areebawaqar007@gmail.com>
2026-07-29 09:58:07 +0500

    complete profile UI page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,21 +5,23 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import  PrivateRoute  from "./components/PrivateRoute";
+
 export default function App() {
   return (
-    <div >
+    <div>
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
         </Routes>
       </BrowserRouter>
-     
     </div>
   );
 }
