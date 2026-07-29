08058f1942a2fb8be05417e3e1fa3285ef052bc2
Areeba Waqar <areebawaqar007@gmail.com>
2026-07-29 07:39:21 +0500

    Add redux persisi

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storageModule from 'redux-persist/lib/storage';
+
+// Get the actual storage object
+const storage = storageModule.default;
+
+console.log(storage);
+
+// Combine all reducers
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+// Redux Persist configuration
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+// Make Redux state persistent
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create Redux store
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
 
-
+// Create persistor
+export const persistor = persistStore(store);
