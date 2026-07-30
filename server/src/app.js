9a4e04b9301c1714ae7edeb7c05bc76eb8d9e97c
Areeba Waqar <areebawaqar007@gmail.com>
2026-07-30 05:56:09 +0500

    Add listing controller and route

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
 
 
 
