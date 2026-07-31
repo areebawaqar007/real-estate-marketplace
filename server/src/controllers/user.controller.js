57d2107afae85f640d6d103084d489c136082358
Areeba Waqar <areebawaqar007@gmail.com>
2026-07-31 09:26:26 +0500

    Complete show user listings functionality

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../Utils/error.js';
-
+import Listing from '../models/listing.model.js';
 export const test = (req, res) => {
   res.json({
     message: 'Api route is working!',
