3a0e506a3c7166bc32b9d0757ce0e2be5207d71c
Areeba Waqar <areebawaqar007@gmail.com>
2026-07-31 08:51:06 +0500

    Create get user listings API route

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -59,6 +59,21 @@ export const signOut = async (req, res, next) => {
     next(error);
   }
 };
+
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
+  }
+};
+
 // MongoDB
 //    ↓
 // User.findByIdAndUpdate()
