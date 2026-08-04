829a1d98cadee4e23e1af6186735cf06ebf112e9
Areeba Waqar <areebawaqar007@gmail.com>
2026-08-05 00:58:04 +0500

    ready to deploy

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -38,6 +38,7 @@ export default function Profile() {
         headers: {
           "Content-Type": "application/json",
         },
+         credentials: 'include',
         body: JSON.stringify(formData),
       });
       const data = await res.json();
